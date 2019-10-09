535fc926de7c596a790fea8352d621c8e23eac40
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2019-10-10 00:23:40 +0100

    Add hoogle in shell

--- default.nix
+++ default.nix
@@ -1,13 +1,23 @@
 # to see ghc versions:
 # nix-instantiate --eval -E "with import ./nix/nixpkgs.nix {}; lib.attrNames haskell.compiler"
-{ pkgs ? import ./nix/nixpkgs.nix { }, compiler ? null }:
+{ pkgs ? import ./nix/nixpkgs.nix { }, compiler ? null, withHoogle ? true }:
 
 let
 
-  haskellPackages = if isNull compiler then
+  haskellPackagesNoHoogle = if isNull compiler then
     pkgs.haskellPackages
   else
     pkgs.haskell.packages.${compiler};
+
+  haskellPackagesWithHoogle = haskellPackagesNoHoogle.override {
+    overrides = (self: super: {
+      ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+      ghcWithPackages = self.ghc.withPackages;
+    });
+  };
+
+  haskellPackages =
+    if withHoogle then haskellPackagesWithHoogle else haskellPackagesNoHoogle;
 
   higgledy-src = pkgs.fetchFromGitHub {
     owner = "i-am-tom";
@@ -27,9 +37,7 @@ let
       alias ghcid-orig="$(which ghcid)"
       alias ghcid="ghcid -a --command='cabal new-repl' --restart=harg.cabal"
     '';
-    buildInputs = [
-      haskellPackages.ghcid
-    ];
+    buildInputs = [ haskellPackages.ghcid ];
   };
 
 in if pkgs.lib.inNixShell then shell else harg
