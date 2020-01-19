0f17b3e0f2eda48b3afafaeb2813c0551e5d681b
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-01-19 16:22:27 +0000

    Move haskell overrides outside of default.nix

--- default.nix
+++ default.nix
@@ -6,11 +6,10 @@ let
 
   nixpkgs = if isNull pkgs then
     import (import ./nix/sources.nix).nixos-stable { }
+  else if builtins.typeOf pkgs == "set" then
+    pkgs
   else
-    if builtins.typeOf pkgs == "set" then
-      pkgs
-    else
-      import (builtins.getAttr pkgs (import ./nix/sources.nix)) {};
+    import (builtins.getAttr pkgs (import ./nix/sources.nix)) { };
 
   inShell = nixpkgs.lib.inNixShell;
 
@@ -31,17 +30,14 @@ let
   else
     haskellPackagesNoHoogle;
 
-  harg-src = nixpkgs.nix-gitignore.gitignoreSource [] ./.;
-  higgledy-src = nixpkgs.fetchFromGitHub {
-    owner = "i-am-tom";
-    repo = "higgledy";
-    rev = "476d73a92e3ef6e1dc879555d751f877b0f91de8";
-    sha256 = "1vg9ha3knggyh5a76678y808c29v9p1mai9bq355q7amy0icy46j";
+  src = nixpkgs.nix-gitignore.gitignoreSource [ ] ./.;
+
+  overrides = import ./nix/overrides.nix {
+    pkgs = nixpkgs;
+    haskellPackages = haskellPackages;
   };
 
-  harg = haskellPackages.callCabal2nix "harg" harg-src {
-    higgledy = haskellPackages.callCabal2nix "higgledy" higgledy-src { };
-  };
+  harg = haskellPackages.callCabal2nix "harg" src overrides;
 
   shell = nixpkgs.mkShell {
     inputsFrom = [ harg.env ];
---
+++ nix/overrides.nix
@@ -0,0 +1,12 @@
+{ pkgs, haskellPackages }:
+
+let
+
+  higgledy-src = pkgs.fetchFromGitHub {
+    owner = "i-am-tom";
+    repo = "higgledy";
+    rev = "476d73a92e3ef6e1dc879555d751f877b0f91de8";
+    sha256 = "1vg9ha3knggyh5a76678y808c29v9p1mai9bq355q7amy0icy46j";
+  };
+
+in { higgledy = haskellPackages.callCabal2nix "higgledy" higgledy-src { }; }
