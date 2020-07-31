4cc4eb647f8939331f2d9265bdf0b138b87a9368
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-07-31 09:02:46 +0100

    Try to make gh-actions work

--- nix/overrides.nix
+++ nix/overrides.nix
@@ -8,5 +8,9 @@ let
     rev = "476d73a92e3ef6e1dc879555d751f877b0f91de8";
     sha256 = "1vg9ha3knggyh5a76678y808c29v9p1mai9bq355q7amy0icy46j";
   };
+  higgledy = self.callCabal2nix "higgledy" higgledy-src {};
 
-in { higgledy = self.callCabal2nix "higgledy" higgledy-src { }; }
+in
+{
+  higgledy = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.dontHaddock higgledy);
+}
