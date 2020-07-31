500da5076595e868e068fc82308749217fbfaefe
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2020-07-31 09:14:28 +0100

    Fix haskellPackages override

--- default.nix
+++ default.nix
@@ -1,36 +1,21 @@
 # to see ghc versions:
 # nix-instantiate --eval -E "with import ./nix/nixpkgs.nix {}; lib.attrNames haskell.compiler"
-{ pkgs ? null, compiler ? null, withHoogle ? true }:
+{ pkgs ? null, compiler ? null }:
 
 let
 
   nixpkgs = if isNull pkgs then
-    import (import ./nix/sources.nix).nixos-stable { }
+    import (import ./nix/sources.nix).nixos-stable {}
   else if builtins.typeOf pkgs == "set" then
     pkgs
   else
-    import (builtins.getAttr pkgs (import ./nix/sources.nix)) { };
+    import (builtins.getAttr pkgs (import ./nix/sources.nix)) {};
 
-  inShell = nixpkgs.lib.inNixShell;
-
-  haskellPackagesNoHoogle = if isNull compiler then
+  haskellPackagesBase = if isNull compiler then
     nixpkgs.haskellPackages
   else
     nixpkgs.haskell.packages.${compiler};
 
-  haskellPackagesWithHoogle = haskellPackagesNoHoogle.override {
-    overrides = (self: super: {
-      ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
-      ghcWithPackages = self.ghc.withPackages;
-    });
-  };
-
-  haskellPackagesBase = if (inShell && withHoogle) then
-    haskellPackagesWithHoogle
-  else
-    haskellPackagesNoHoogle;
-
-  # src = nixpkgs.nix-gitignore.gitignoreSource [ ] ./.;
   srcRegex = [
     "^docs.*$"
     "^harg.cabal$"
@@ -40,23 +25,32 @@ let
     "^src.*$"
     "^test.*$"
   ];
-  src = nixpkgs.lib.sourceByRegex ./. srcRegex;
-  drv = haskellPackages.callCabal2nix "harg" src { };
 
   haskellPackages = haskellPackagesBase.override {
     overrides = self: super:
       let
-        packages = import ./nix/overrides.nix {
+        hsPkgs = import ./nix/overrides.nix {
           pkgs = nixpkgs;
           self = self;
           super = super;
         };
-      in packages // { "harg" = drv; };
+        src = nixpkgs.lib.sourceByRegex ./. srcRegex;
+        drv = self.callCabal2nix "harg" src {};
+      in
+        hsPkgs // { harg = drv; };
   };
 
-  shell = nixpkgs.mkShell {
-    inputsFrom = [ haskellPackages.harg.env ];
-    buildInputs = [ haskellPackages.ghcid haskellPackages.cabal-install ];
+  shell = haskellPackages.shellFor {
+    packages = ps: [ ps.harg ];
+    buildInputs = [
+      haskellPackages.ghcid
+      haskellPackages.cabal-install
+    ];
+    withHoogle = true;
   };
 
-in if inShell then shell else haskellPackages.harg
+in
+
+if nixpkgs.lib.inNixShell
+then shell
+else haskellPackages.harg
